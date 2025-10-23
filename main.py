dca709854a2fcc82f559ef4416e50f7c4e07be4e
NikosKl <nikos.klavi@gmail.com>
2025-10-23 18:38:57 +0300

    Addec a connection between products and suppliers. Added report_manager.py with  report options about products, stock and suppliers

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sqlite3
+import pandas as pd
 from product_manager import get_valid_input, add_product, update_product, delete_product, show_all
 from sales_manager import record_sale, show_sales
-from supplier_manager import add_supplier, delete_supplier, update_supplier, show_all_suppliers
+from supplier_manager import add_supplier, delete_supplier, update_supplier, show_all_suppliers, assign_products_to_supplier
+from reports_manager import total_revenue, best_seller, total_orders, stock_report, low_stock_report, active_suppliers, supplier_product_coverage, products_without_supplier
 
 connection = sqlite3.connect('inventory.sqlite')
 cur = connection.cursor()
@@ -12,7 +14,8 @@ while True:
         '\nEnter (1) - Products'
         '\nEnter (2) - Sales'
         '\nEnter (3) - Suppliers'
-        '\nEnter (4) - Save/Exit ',['1','2','3','4'])
+        '\nEnter (4) - Report & Analytics'
+        '\nEnter (5) - Exit ',['1','2','3','4','5'])
     if user_input == '1':
         print('\n--- Product Menu ---')
         products_menu = get_valid_input(
@@ -43,15 +46,57 @@ while True:
             '\nEnter (1) - Add a Supplier'
             '\nEnter (2) - Update a Supplier'
             '\nEnter (3) - Delete a Supplier'
-            '\nEnter (4) - Show all Suppliers ',['1','2','3','4'])
+            '\nEnter (4) - Assign products to Suppliers'
+            '\nEnter (5) - Show all Suppliers ',['1','2','3','4', '5'])
         if suppliers_menu == '1':
             add_supplier(cur)
         elif suppliers_menu == '2':
             update_supplier(cur)
         elif suppliers_menu == '3':
             delete_supplier(cur)
+        elif suppliers_menu == '4':
+            assign_products_to_supplier(cur, pd)
         else:
             show_all_suppliers(cur)
+    elif user_input == '4':
+        print('\n--- Reports & Analytics Menu ---')
+        reports_menu = get_valid_input(
+            '\nEnter (1) - Sales Report'
+            '\nEnter (2) - Inventory Report'
+            '\nEnter (3) - Supplier Report ',['1','2','3'])
+        if reports_menu == '1':
+            print('\n--- Sales Reports Menu ---')
+            sales_report_menu = get_valid_input(
+                '\nEnter (1) - Total Sales Revenue'
+                '\nEnter (2) - Best Seller products'
+                '\nEnter (3) - Total Orders ',['1','2','3'])
+            if sales_report_menu == '1':
+                total_revenue(pd, connection)
+            elif sales_report_menu == '2':
+                best_seller(pd, connection)
+            else:
+                total_orders(cur)
+        elif reports_menu == '2':
+            print('\n--- Inventory Report Menu ---')
+            inventory_reports_menu = get_valid_input(
+                '\nEnter (1) - Inventory Stock Report'
+                '\nEnter (2) - Low Stock Report ',['1','2'])
+            if inventory_reports_menu == '1':
+                stock_report(pd, connection)
+            else:
+                low_stock_report(pd, connection)
+        elif reports_menu == '3':
+            print('\n--- Supplier Report Menu ---')
+            supplier_report_menu = get_valid_input(
+                '\nEnter (1) - Supplier info'
+                '\nEnter (2) - Products per Supplier'
+                '\nEnter (3) - Show products without Supplier ',['1','2','3'])
+            if supplier_report_menu == '1':
+                active_suppliers(connection,pd)
+            elif supplier_report_menu == '2':
+                supplier_product_coverage(connection, pd)
+            else:
+                products_without_supplier(connection, pd)
     else:
         exit_confirmation = get_valid_input('\nAre you sure you want to exit? (y/n): ',['y','n'])
         if exit_confirmation == 'y':
@@ -59,4 +104,4 @@ while True:
             cur.close()
             break 
         else:
-            continue
+            pass
--- supplier_manager.py
+++ supplier_manager.py
@@ -1,5 +1,6 @@
-from product_manager import get_valid_input, get_alphabetic_input, get_positive_number
+from product_manager import get_valid_input, get_positive_number
 from email_validator import validate_email, EmailNotValidError
+from tabulate import tabulate
 
 def get_valid_email():
     while True:
@@ -10,8 +11,19 @@ def get_valid_email():
         except EmailNotValidError as error:
             print('Invalid Email: ', error)
 
+def products_ids_input():
+    while True:
+        product_id = input("\nEnter product id's to assign (comma-separated): ")
+        product_ids = [p_id.strip() for p_id in product_id.split(',') if p_id.strip().isdigit()]
+
+        if not product_ids:
+            print('\nNo valid product ID entered.')
+        else:
+            product_ids = list(map(int, product_ids))
+            return product_ids
+
 def add_supplier(cur):
-    supplier_name = input('\nEnter suppliers name: ')
+    supplier_name = input('\nEnter suppliers name: ').strip()
     supplier_contact = get_valid_email()
     cur.execute('INSERT OR IGNORE INTO Suppliers (name, contact) VALUES (?,?)',(supplier_name, supplier_contact))
     print('\nSupplier has been added successfully!')
@@ -41,7 +53,7 @@ def update_supplier(cur):
     else:
         update_choice = get_valid_input('\nChoose which value you want to update (Name/Contact): ',['name','contact'])
         if update_choice == 'name':
-            supplier_name = input('\nPlease type a new name: ')
+            supplier_name = input('\nPlease type a new name: ').strip()
             cur.execute('UPDATE Suppliers SET name = ? WHERE id = ?',(supplier_name, row[0]))
             print('\nSupplier name has been updated!')
             cur.connection.commit()
@@ -56,3 +68,39 @@ def show_all_suppliers(cur):
     cur.execute('SELECT * FROM SUPPLIERS')
     for rows in cur.fetchall():
         print(f'Supplier ID: {rows[0]} | Supplier Name: {rows[1]} | Supplier Contact: {rows[2]}')
+
+def assign_products_to_supplier(cur, pd):
+    supplier_data = pd.read_sql_query('SELECT name FROM SUPPLIERS', cur.connection)
+    print("\nThe active suppliers are:\n")
+    print(tabulate(supplier_data, headers='keys', tablefmt='simple_grid', showindex=False))
+
+    valid_suppliers = supplier_data['name'].str.lower().tolist()
+    user_input = get_valid_input('\nSelect a supplier to assign products: ',valid_suppliers)
+
+    cur.execute('SELECT id FROM Suppliers WHERE LOWER(name) = ?', (user_input.lower(), ))
+    supplier_id = cur.fetchone()
+    if supplier_id is None:
+        print('\nSupplier not found.')
+        return
+    else:
+        supplier_id = supplier_id[0]
+
+        data = pd.read_sql_query('SELECT * FROM PRODUCTS', cur.connection)
+        print('\n' + tabulate(data, headers='keys', tablefmt='simple_grid', showindex=False))
+        assigned_products = products_ids_input()
+        if not assigned_products:
+            print('\nNo products were assigned to supplier.')
+            return
+        else:
+            updated = False
+
+            for id in assigned_products:
+                if id in data['id'].values:
+                    cur.execute('INSERT OR IGNORE INTO SupplierProducts (supplier_id, product_id) VALUES (?,?)',(supplier_id, id)) 
+                    updated = True
+                else:
+                    print("\nProduct id doesn't exist")
+
+            if updated:
+                print("\nID's have been updated!")
+                cur.connection.commit()
